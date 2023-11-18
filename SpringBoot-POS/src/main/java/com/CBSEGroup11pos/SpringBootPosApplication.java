9039997a63d3fe3d881ccab79503f5af62147a7c
LimZiXuan <limzixuan1@gmail.com>
2023-11-18 17:07:18 +0800

    Initial Commit

---
+++ SpringBoot-POS/src/main/java/com/CBSEGroup11pos/SpringBootPosApplication.java
@@ -0,0 +1,13 @@
+package com.CBSEGroup11pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPosApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-POS/src/test/java/com/CBSEGroup11pos/SpringBootPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CBSEGroup11pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
