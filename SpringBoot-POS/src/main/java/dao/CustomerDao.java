ec2fda7e329d476ff48d5259f2f94c499f5aa1f4
LimZiXuan <limzixuan1@gmail.com>
2024-01-06 10:21:11 +0800

    delete Customer

--- SpringBoot-POS/src/main/java/dao/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import entity.Customer;
-public interface CustomerDao extends JpaRepository<Customer, String> { 
-	List<Customer> findAll();
-}
